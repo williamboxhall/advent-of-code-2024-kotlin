b7fb97e573ba32e29c60765875c707820dd710c2
William Boxhall <william.boxhall@cultureamp.com>
2024-12-19 15:46:47 +1100

    Reinstate part 2 solution which got lost somehow

--- src/Day01.kt
+++ src/Day01.kt
@@ -10,12 +10,21 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val split = input.map { it.split("   ").map { it.trim().toInt() } }
+        val first = split.map { it.first() }.sorted()
+        val second = split.map { it.last() }.sorted()
+
+        val secondIncidence = second.groupingBy { it }.eachCount()
+        val scores = first.map { it * (secondIncidence[it]?:0) }
+        val summed = scores.sum()
+
+        return summed
     }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
