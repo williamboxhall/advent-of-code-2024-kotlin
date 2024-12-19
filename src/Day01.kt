e381b1c23a7cd2eb8f1ed5a20921a7338f4c4d9e
William Boxhall <william.boxhall@cultureamp.com>
2024-12-19 15:36:23 +1100

    Day 1 complete

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,21 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val split = input.map { it.split("   ").map { it.trim().toInt() } }
+        val first = split.map { it.first() }.sorted()
+        val second = split.map { it.last() }.sorted()
+        val zipped = first.zip(second)
+        val diffed = zipped.map { Math.abs(it.second - it.first) }
+        val summed = diffed.sum()
+        return summed
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
